c05d1a610070dd41cad1ccc149031eb8e0f40b93
ben awad <benawad97@gmail.com>
2017-02-28 18:32:19 -0600

    Figure out epic for signup

--- src/epics/index.js
+++ src/epics/index.js
@@ -1,13 +1,14 @@
-import { ajax } from 'rxjs/observable/dom/ajax';
+import { fromPromise } from 'rxjs/observable/fromPromise';
+import { Observable } from 'rxjs/Observable';
 import { combineEpics } from 'redux-observable';
 
-import superagent from 'superagent';
+import axios from 'axios';
 import feathers from 'feathers-client';
 import rest from 'feathers-rest/client';
 
 const host = 'http://5f020b7a.ngrok.io';
 const app = feathers()
-  .configure(rest(host).superagent(superagent))
+  .configure(rest(host).axios(axios))
   .configure(feathers.hooks())
   .configure(feathers.authentication({ storage: window.localStorage }));
 
@@ -16,8 +17,15 @@ const users = app.service('users');
 const signupEpic = action$ =>
   action$.ofType('SIGNUP_REQUESTED')
     .mergeMap(action =>
-      users.create(action.payload)
-        .map(response => ({type: 'Yo'}))
+      fromPromise(users.create(action.payload))
+        .map(response => ({
+          type: 'SIGNUP_SUCCEEDED',
+          response
+        }))
+        .catch(error => Observable.of({
+          type: 'SIGNUP_FAILED',
+          error
+        }))
     );
 
 export default combineEpics(
